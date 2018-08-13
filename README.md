dc4bc9046ed670ada2e33f8f889623bacffafc64
BAlter <barnaby.alter@nyu.edu>
2018-08-13 11:59:09 -0400

    Updated spec for rspec3 syntax

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 [![Build Status](https://travis-ci.org/NYULibraries/wtf_sfx.png?branch=master)](https://travis-ci.org/NYULibraries/wtf_sfx)
 
-Seriously, what the fuck?  Sometimes the SFX API tanks on Pubmed IDs.
+Sometimes the SFX API tanks on Pubmed IDs.
 This suite of tests intends to get us some data on it.
--- spec/wtf_sfx/pmid_spec.rb
+++ spec/wtf_sfx/pmid_spec.rb
@@ -4,10 +4,10 @@ module WtfSfx
     describe "Pubmed resolution for pmid #{pmid} in the SFX API", type: :feature do
       subject(:api_url_with_pmid) { "#{API_URL}&rft_id=info%3Apmid%2F#{pmid}" }
       # Print out the HTML if we don't find any full text
-      after(:each) do
+      after(:each) do |example|
         unless example.exception.nil?
           p "SFX API response for #{api_url_with_pmid}\n"
-          p page.html 
+          p page.html
         end
       end
       it "has a full text service" do
